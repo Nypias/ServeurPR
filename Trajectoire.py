00d049c84fd102b83054764a9fece64cf1e65b41
ClementNotin <clement@notin.org>
2011-03-04 21:08:08 +0100

    création de Trajectoire ATTENTION : ce module est destiné à être lancé seul ("python Trajectoire.py") et pas à être utilisé par le reste du projet C'est juste un premier jet qui affiche la trajectoire au lieu de la sauvegarder pour l'envoyer aux joueurs par exemple. De plus elle ne gère pas les raquettes, et est limité à un jeu à 2 joueurs.
    J'ai commit ce test pour montrer à rclaude ce que j'avais fait

--- Trajectoire.py
+++ Trajectoire.py
@@ -1,7 +1,30 @@
 # -*- coding: utf-8 -*-
 
-""" M�thode de calcul de trajectoire, doit impl�menter une m�thode pour
+import random, math
+
+""" Méthode de calcul de trajectoire, doit implémenter une méthode pour
 transformer la trajectoire en string JSON """
 class Trajectoire:
-	def __init__(self):
-		pass
+    def __init__(self):
+        angle = random.random()*360
+        print "orientation départ = %f" % angle
+        self.ball = [50,50]
+        print ("%f;%f" % (self.ball[0], self.ball[1])).replace(".", ",")
+
+        for i in range(10):
+            self.ball[0] = self.ball[0] + math.cos(math.radians(angle))
+            self.ball[1] = self.ball[1] - math.sin(math.radians(angle)) # "-" car l'axe des Y est vers le bas
+            while(self.ball[0] > 0 and self.ball[0] < 100 and self.ball[1] > 0 and self.ball[1] < 100):
+                self.ball[0] = self.ball[0] + math.cos(math.radians(angle))
+                self.ball[1] = self.ball[1] - math.sin(math.radians(angle)) # "-" car l'axe des Y est vers le bas
+            self.ball[0] = self.ball[0]
+            self.ball[1] = self.ball[1]
+            if (self.ball[0] <= 0 or self.ball[0] >= 100): #si x = 0 ou 100 => collision sur un bord vertical (// axe y) => a' = 180 - a
+                angle = 180 - angle
+            elif (self.ball[1] <= 0 or self.ball[1] >= 100): #si y = 0 ou 100 => collision sur un bord horizontal (// axe x) => a' = - a
+                angle = 360 - angle
+            print ("%f;%f" % (self.ball[0], self.ball[1])).replace(".", ",")
+
+
+
+traj = Trajectoire()
