dda9c2e0e7b32e6b0d617b24c359cc0dbf6e61c5
rclaude <charlesalbert.claude@gmail.com>
2011-05-06 15:48:57 +0200

    Modification on Jeu.py: ajouterJoueur(self,joueur) became dynamic. Modification on Trajectoire.py: useless for now No regression for two players: OK

--- Trajectoire.py
+++ Trajectoire.py
@@ -18,26 +18,36 @@ class Trajectoire :
 
         self.jeu = jeu;
         self.joueurs = self.jeu.joueurs
-        time = 0
-        angle = random.random()*360
-        self.ball = [50, 50]
-        self.ball[0] = self.ball[0] + math.cos(math.radians(angle))
-        self.ball[1] = self.ball[1] - math.sin(math.radians(angle)) # "-" car l'axe des Y est vers le bas
-        while(self.ball[0] > 0 and self.ball[0] < 100 and self.ball[1] > 0 and self.ball[1] < 100):
-            self.ball[0] = self.ball[0] + math.cos(math.radians(angle))
-            self.ball[1] = self.ball[1] - math.sin(math.radians(angle)) # "-" car l'axe des Y est vers le bas
-            #print self.ball[0], self.ball[1]
-            time += Trajectoire.TIME_INT
-        self.ball[0] = self.ball[0]
-        self.ball[1] = self.ball[1]
+        if jeu.nbJoueurs <= 2:
+			time = 0
+			angle = random.random()*360
+			self.ball = [50, 50]
+			self.ball[0] = self.ball[0] + math.cos(math.radians(angle))
+			self.ball[1] = self.ball[1] - math.sin(math.radians(angle)) # "-" car l'axe des Y est vers le bas
+			while(self.ball[0] > 0 and self.ball[0] < 100 and self.ball[1] > 0 and self.ball[1] < 100):
+				self.ball[0] = self.ball[0] + math.cos(math.radians(angle))
+				self.ball[1] = self.ball[1] - math.sin(math.radians(angle)) # "-" car l'axe des Y est vers le bas
+				#print self.ball[0], self.ball[1]
+				time += Trajectoire.TIME_INT
+			self.ball[0] = self.ball[0]
+			self.ball[1] = self.ball[1]
 
-        pointCollision = (self.ball[0], self.ball[1])
-        
-        print "COLLISION dans " + str(time) + " avec positionCollision = " + str(pointCollision)
-        
-        self.sendPoint(pointCollision,time)
-        reactor.callLater(time, self.choisirTrajectoire, pointCollision, angle)
-        
+			pointCollision = (self.ball[0], self.ball[1])
+			
+			print "COLLISION dans " + str(time) + " avec positionCollision = " + str(pointCollision)
+			
+			self.sendPoint(pointCollision,time)
+			reactor.callLater(time, self.choisirTrajectoire, pointCollision, angle)
+        elif jeu.nbJoueurs > 2:
+            # creation of the field
+            angle = 360/jeu.nbJoueurs
+            self.Xfield[50,90] # list of the ordinate starting with the field center and next the field corners
+            self.Yfield[50,50] # list of the abscissa ...
+            i = 1
+            while i<jeu.nbJoueurs:
+                self.Xfield.append(50 + 40*math.cos(angle*i)) # trunc isn't needed, python is so high
+                self.Yfield.append(50 + 40*math.sin(angle*i))
+                self.Xball[90,0] 
         
     def sendPoint(self, point,temps):
         
