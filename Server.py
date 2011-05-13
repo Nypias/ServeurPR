1aaa62b4b5e448bacf6a14b5222eae987650f28d
ClementNotin <clement@notin.org>
2011-05-13 11:01:22 +0200

    On peut se passer du serveur Apache maintenant, passage de php à html en utilisant les fonctionnalites disponibles dans twisted.web

--- Server.py
+++ Server.py
@@ -3,8 +3,22 @@
 from Jeu import JeuFactory
 from Joueur import Joueur
 from twisted.web.static import File
+from twisted.web.resource import Resource
 from websocket import *
 import webbrowser #TESTING purpose
+import cgi
+
+
+class GamePage(Resource):
+    def render_GET(self, request):
+        return '<a href="index.html">Aller a l\'accueil du jeu</a>'
+
+    def render_POST(self, request):
+        pageFile = open("../ClientPR/pongroulette.html")
+        page = pageFile.read()
+        page = page.replace("$PSEUDO$", cgi.escape(request.args["nomdujoueur"][0]))
+        pageFile.close()        
+        return page
 
 
 if __name__ == "__main__":
@@ -13,6 +27,7 @@ if __name__ == "__main__":
     #WebSocketSite = une factory !
     #Simple() est une ressource
     root = File("../ClientPR/")
+    root.putChild("gamePage", GamePage())
     factory = JeuFactory(root)
     factory.addHandler('/game', Joueur)
     reactor.listenTCP(8080, factory)
